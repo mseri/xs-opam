63a9c17d385ed0b72b91b127839b4e030ca57e4b
Marcello Seri <marcello.seri@citrix.com>
2018-02-09 17:26:33 +0000

    travis: pass the variables to the docker container
    Signed-off-by: Marcello Seri <marcello.seri@citrix.com>

--- opam-update.sh
+++ opam-update.sh
@@ -1,6 +1,6 @@
 sudo yum install python-requests -y
 
-if python tools/opam-updates.py --ignore-updates --auth $USER:$TOKEN \
+if python tools/opam-updates.py --ignore-updates --auth $GH_USER:$GH_TOKEN \
     | grep -q "Nothing to do, xs-opam is already up to date.";
 then
     exit 0
