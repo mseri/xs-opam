9ed02355e1fcab6d81ea22edc2542c1d2116f7f2
Marcello Seri <marcello.seri@citrix.com>
2018-02-14 13:53:40 +0000

    opam-update: update to show output
    Signed-off-by: Marcello Seri <marcello.seri@citrix.com>

--- opam-update.sh
+++ opam-update.sh
@@ -1,7 +1,8 @@
 sudo yum install python-requests -y
 
-if python tools/opam-updates.py --ignore-updates --auth $GH_USER:$GH_TOKEN \
-    | grep -q "Nothing to do, xs-opam is already up to date.";
+python tools/opam-updates.py . --ignore-updates --ignore-travis-opam --auth $GH_USER:$GH_TOKEN | tee /tmp/opam-updates.out
+
+if cat /tmp/opam-updates.out | grep -q "Nothing to do";
 then
     exit 0
 fi
