6fad1a7222f41dfd3ea845ef92566ce04f2d50b9
Marcello Seri <mseri@users.noreply.github.com>
2018-03-17 11:37:04 +0000

    Update opam-update.sh

--- opam-update.sh
+++ opam-update.sh
@@ -1,6 +1,6 @@
 sudo yum install python-requests -y
 
-python tools/opam-updates.py . --ignore-updates --ignore-travis-opam --auth $GH_USER:$GH_TOKEN | tee /tmp/opam-updates.out
+python tools/opam-updates.py --ignore-updates --ignore-travis-opam --auth $GH_USER:$GH_TOKEN | tee /tmp/opam-updates.out
 
 if cat /tmp/opam-updates.out | grep -q "Nothing to do";
 then
